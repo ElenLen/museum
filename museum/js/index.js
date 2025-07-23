6d1c5b089b0957f2795ba668911cab0fc4c3f0ea
Elena Petukhova <relax-pl@yandex.ru>
2025-07-23 23:16:06 +0500

    feat: add burger menu

--- museum/js/index.js
+++ museum/js/index.js
@@ -1,14 +1,37 @@
 $(document).ready(function () {
+  const burgerMenu = document.getElementById("burger");
+  const mobileMenu = document.getElementById("menu");
+  const closeMenu = document.getElementById("close");
+
+  burgerMenu.addEventListener("click", function () {
+    burgerMenu.classList.toggle("open");
+    mobileMenu.classList.toggle("open");
+    burgerMenu.style.display = "none";
+    closeMenu.style.display = "block";
+  });
+
+  closeMenu.addEventListener("click", function () {
+    burgerMenu.classList.remove("open");
+    mobileMenu.classList.remove("open");
+    burgerMenu.style.display = "block";
+    closeMenu.style.display = "none";
+  });
+
+  mobileMenu.addEventListener("click", function () {
+    burgerMenu.classList.remove("open");
+    mobileMenu.classList.remove("open");
+    burgerMenu.style.display = "block";
+    closeMenu.style.display = "none";
+  });
+
   let slideCenter = $(".welcome-image");
   slideCenter.slick({
     infinite: true,
     slidesToShow: 1,
   });
 
-  // вывод номера текущего слайдера в span
   let numSlider = $("#numSlider");
   $(".slick-arrow").click(function () {
-    // console.log(slideCenter.slick('slickCurrentSlide') + 1);
     numSlider.text(slideCenter.slick("slickCurrentSlide") + 1);
   });
 });
